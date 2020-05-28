8c9abdf9b39057f42e5d0d41a0bcb0be74ea0dcd
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2020-05-28 15:17:22 -0700

    Conan package setup

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,16 @@
 from conans import ConanFile
 
 class SliceConan(ConanFile):
-    name = "hn-conan"
+    name = "slice"
+    version = "0.1.0"
+    url = "https://github.com/hnOsmium0001/slice"
+    description = "Container slice library for C++14 and above"
+    license = "CC0-1.0"
+    exports_sources = "include/*"
+    no_copy_source = True
+
+    def package(self):
+        self.copy("*.h")
+
+    def package_id(self):
+        self.info.header_only()
